b169daf0053cc303b7f60466959612fc7c5f3be5
JYOTSHANA S R <115572761+jyotshana666@users.noreply.github.com>
2026-09-25 19:00:46 +0530

    Bulk sync: 9 submissions

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int res = nums[0];
+        int maxProd = nums[0];
+        int minProd = nums[0];
+
+        for(int i=1; i<nums.length; i++) {
+            int curr = nums[i];
+
+            if(curr < 0) {
+                int temp = maxProd;
+                maxProd = minProd;
+                minProd = temp;
+            }
+
+            maxProd = Math.max(curr, maxProd * curr);
+            minProd = Math.min(curr, minProd * curr);
+
+            res = Math.max(res, maxProd);
+        }
+        return res;
+    }
+}
